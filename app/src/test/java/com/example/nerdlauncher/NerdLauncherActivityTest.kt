e878de1cb948d818e169f818ab81d9a4def7ac7d
lindrid <lindrid32@gmail.com>
2021-07-06 20:51:02 -0400

    init

---
+++ app/src/test/java/com/example/nerdlauncher/NerdLauncherActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.nerdlauncher
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import junit.framework.TestCase
+import org.junit.Test
+
+class NerdLauncherActivityTest : TestCase() {
+  private val subject: NerdLauncherActivity = NerdLauncherActivity()
+
+  public override fun setUp() {
+    super.setUp()
+  }
+}
